7300ec10375818087eb7ea2f3fac9040122bb56b
Kirill Vnukov <kirillgrandson@gmail.com>
2023-09-12 23:20:37 +0300

    Finish

--- main.php
+++ main.php
@@ -11,10 +11,7 @@ if (isset($_GET['date_field_id']) && isset($_GET['status_lead_id'])) {
 
     $sol = new Solution('https://api.s1.yadrocrm.ru/tmp', '23bc075b710da43f0ffb50ff9e889aed');
     $data = $sol->calculateCounts($dateFieldId, $statusLeadIds);
-
-    header('Content-Type: application/json; charset=utf-8');
     echo json_encode($data);
 } else {
-    header('Content-Type: application/json; charset=utf-8');
     echo json_encode(['message' => 'Введите ID доп. поля с датой и ID статуса сделки (несколько).']);
 }
